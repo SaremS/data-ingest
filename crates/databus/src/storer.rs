cc86fc057bf1979d1bc474a41371921a42d808aa
Sarem Seitz <sarem.seitz@gmail.com>
2026-06-02 19:24:23 +0200

    clippy fix

--- crates/databus/src/storer.rs
+++ crates/databus/src/storer.rs
@@ -125,7 +125,7 @@ impl<
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::message::{MessageHeader, MessageType};
+    
     use std::sync::Arc;
     use tokio::sync::Mutex;
     use tokio::time::{Duration, sleep};
